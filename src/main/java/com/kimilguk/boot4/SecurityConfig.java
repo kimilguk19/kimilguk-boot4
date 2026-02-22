341647d79a1722d11572de256b081d833e8b2638
kimilguk <kimilguk19@gmail.com>
2026-02-22 12:13:03 +0900

    02주차 1교시 강의소스

--- src/main/java/com/kimilguk/boot4/SecurityConfig.java
+++ src/main/java/com/kimilguk/boot4/SecurityConfig.java
@@ -8,11 +8,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable()); // Disable CSRF protection
-
-        return http.build();
-    }
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable()) // CSRF 비활성화
+				.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // 모든 요청 허용
+		return http.build();
+	}
 }
