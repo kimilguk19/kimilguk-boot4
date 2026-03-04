3cdb6012c0a338abf1cafe76673a089de61c7459
kimilguk <kimilguk19@gmail.com>
2026-03-04 12:19:59 +0900

    4주차 2교시 강의소스

--- src/main/java/com/kimilguk/boot4/web/IndexController.java
+++ src/main/java/com/kimilguk/boot4/web/IndexController.java
@@ -20,6 +20,11 @@ public class IndexController {
 	//로그 출력 객체생성
     private Logger logger = LoggerFactory.getLogger(getClass());
     private final PostsService postsService;//생성자로 주입
+    
+    @GetMapping("/posts/save")//Url주소와 posts-save.mustache를 매핑 시킨다.
+    public String postsSave() {
+        return "posts/posts-save";
+    }
 
     @GetMapping("/")//전체게시물 Read 접근 Api Url을 도메인 루트로 변경한다
     public String postList(@PageableDefault(size=5,sort="id",direction=Sort.Direction.DESC) Pageable pageable, Model model) {
