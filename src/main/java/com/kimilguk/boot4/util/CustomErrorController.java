a7a5958aab151882dd4ca65eb7941afcdac71115
kimilguk <kimilguk19@gmail.com>
2026-03-12 10:46:30 +0900

    6주차 1교시 강의소스

--- src/main/java/com/kimilguk/boot4/util/CustomErrorController.java
+++ src/main/java/com/kimilguk/boot4/util/CustomErrorController.java
@@ -7,8 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
