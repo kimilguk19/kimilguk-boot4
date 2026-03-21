6ad0ad7a85089790ea2fa8b53115c8d29b922bfa
kimilguk <kimilguk19@gmail.com>
2026-03-21 15:17:51 +0900

    10주차 2교시 강의소스

--- src/main/java/com/kimilguk/boot4/util/ScriptUtils.java
+++ src/main/java/com/kimilguk/boot4/util/ScriptUtils.java
@@ -19,4 +19,18 @@ public class ScriptUtils {
         out.println("<script>alert('"+alertText+"');location.href='"+nextPage+"';</script>");
         out.flush();//객체삭제
     }// 페이지 이동 없는 메시지 출력코딩과 메시지 출력 후 이전페이지로 이동 코딩은 다음 시간에 이어서…
+    //메시지 출력 후 페이지 이동 없는 처리(아래)
+    public static void alert(HttpServletResponse response, String alertText) throws IOException {
+        init(response);
+        PrintWriter out = response.getWriter();//스트림 네트워크 데이터처리방식 객체생성 :예외처리필수
+        out.println("<script>alert('"+alertText+"');</script>");
+        out.flush();//객체삭제
+    }
+    //메시지 출력 후 이전 페이지 이동 처리(아래)
+    public static void alertAndBackPage(HttpServletResponse response, String alertText) throws IOException {
+        init(response);
+        PrintWriter out = response.getWriter();//스트림 네트워크 데이터처리방식 객체생성 :예외처리필수
+        out.println("<script>alert('"+alertText+"');history.go(-1)</script>");
+       out.flush();//객체삭제
+    }
 }
--- src/main/java/com/kimilguk/boot4/web/SimpleUsersController.java
+++ src/main/java/com/kimilguk/boot4/web/SimpleUsersController.java
@@ -1,5 +1,7 @@
 package com.kimilguk.boot4.web;
 
+import java.io.IOException;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -13,8 +15,10 @@ import com.kimilguk.boot4.config.auth.LoginUser;
 import com.kimilguk.boot4.config.auth.dto.SessionUser;
 import com.kimilguk.boot4.domain.simple_users.SimpleUsers;
 import com.kimilguk.boot4.service.simple_users.SimpleUsersService;
+import com.kimilguk.boot4.util.ScriptUtils;
 import com.kimilguk.boot4.web.dto.SimpleUsersDto;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor//final 매개변수가 있는 생성자 메소드가 자동 생성된다(객체초기화 역할)
@@ -32,10 +36,21 @@ public class SimpleUsersController {
     }
 
     @PostMapping("/simple_users/save")//회원생성 API실행
-    public String simpleUsersSavePost(SimpleUsersDto requestDto) {
-        simpleUsersService.save(requestDto);
-        return "redirect:/simple_users/list";
-    } //저장 후 URL기준 절대경로로 페이지이동
+    public String simpleUsersSavePost(HttpServletResponse response,SimpleUsersDto requestDto) throws IOException {
+        SimpleUsersDto usersDto = null;
+        try { //아래 구문에서 test회원 등록 시 에러를 발생 시켜보고 try~catch 문을 사용하는 방법을 확인한다.
+            usersDto = simpleUsersService.findByName(requestDto.getUsername());
+        }catch(Exception e){
+        }
+        if(usersDto == null) { //조건추가
+            simpleUsersService.save(requestDto);
+            ScriptUtils.alertAndMovePage(response, "저장 되었습니다.", "/simple_users/list");
+        }else {
+        	System.out.println("중복 아이디 존재");
+            ScriptUtils.alertAndBackPage(response, "중복 아이디가 존재합니다. 아이디를 다시 입력해 주세요.");
+        }
+        return null;//"redirect:/simple_users/list";
+    }//저장 후 절대경로로 페이지이동
    // …중략 이전 장  이어서…
     @GetMapping("/simple_users/list")//회원목록 디자인보기
     public String simpleUsersList(@PageableDefault(size=5,sort="id",direction=Sort.Direction.DESC) Pageable pageable, Model model,@LoginUser SessionUser user) {
@@ -62,15 +77,17 @@ public class SimpleUsersController {
     }
 
     @PostMapping("/simple_users/update")//회원수정 API실행
-    public String simpleUsersUpdatePost(SimpleUsersDto requestDto) {
+    public String simpleUsersUpdatePost(HttpServletResponse response,SimpleUsersDto requestDto) throws IOException {
         simpleUsersService.update(requestDto.getId(), requestDto);
-        return "redirect:/simple_users/update/"+requestDto.getUsername();
+        ScriptUtils.alertAndMovePage(response, "수정 되었습니다.", "/simple_users/update/"+requestDto.getUsername());
+        return null;//"redirect:/simple_users/update/"+requestDto.getUsername();
     }
 
     @PostMapping("/simple_users/delete")//회원삭제 API실행
-    public String simpleUsersDelete(SimpleUsersDto requestDto) {
+    public String simpleUsersDelete(HttpServletResponse response,SimpleUsersDto requestDto) throws IOException {
         simpleUsersService.delete(requestDto.getId());
-        return "redirect:/simple_users/list";//삭제 후 절대경로로 페이지 이동
+        ScriptUtils.alertAndMovePage(response, "삭제 되었습니다.", "/simple_users/list");
+        return null;//"redirect:/simple_users/list";//삭제 후 절대경로로 페이지 이동
     }
 
  }
